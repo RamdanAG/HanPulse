6f74b439ac089c70b0e3680242e87276e5dc0357
Kwanzaa <arsalfahrullh6@gmail.com>
2026-06-01 10:47:22 +0700

    test

--- APP/lib/services/api_service.dart
+++ APP/lib/services/api_service.dart
@@ -3,8 +3,20 @@ import 'package:dio/dio.dart';
 class ApiService {
   final dio = Dio(
     BaseOptions(
-      baseUrl: "",
-      sendTimeout: Duration(seconds: 10)
+      baseUrl: "http://127.0.0.1:8000/api",
+      sendTimeout: Duration(seconds: 20),
+      receiveTimeout: Duration(seconds: 20),
     )
   );
+
+  ApiService(){}
+
+  Future<Map<String, dynamic>> login(String email, String password)async{
+    try{
+      final response = await dio.post("/login", data: {
+        "email": email,
+        "password": password
+      });
+    }
+  }
 }
