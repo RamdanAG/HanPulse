e68124dae5437c476926014033eae58d3f839fd1
Kwanzaa <arsalfahrullh6@gmail.com>
2026-06-01 18:32:05 +0700

    test

--- APP/lib/services/api_service.dart
+++ APP/lib/services/api_service.dart
@@ -1,4 +1,6 @@
 import 'package:dio/dio.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:message/models/user.dart';
 
 class ApiService {
   final dio = Dio(
@@ -9,7 +11,18 @@ class ApiService {
     )
   );
 
-  ApiService(){}
+  ApiService(){
+    dio.interceptors.add(InterceptorsWrapper(
+      onRequest: (options, handler) async{
+        final key = await SharedPreferences.getInstance();
+        final token = key.getString("token");
+        if(token != null){
+          options.headers['Authorization'] = "Bearer $token";
+        }
+        handler.next(options);
+      },
+    ));
+  }
 
   Future<Map<String, dynamic>> login(String email, String password)async{
     try{
@@ -17,6 +30,110 @@ class ApiService {
         "email": email,
         "password": password
       });
+
+      if(response.data['success'] == true && response.statusCode == 200){
+        final key = await SharedPreferences.getInstance();
+        await key.setString("token", response.data['data']['token']);
+        await key.setBool("statusLogin", true);
+      }
+      return response.data;
+    }on DioException catch(e){
+      return{
+        "success": false,
+        "message": e.response?.data['message'].toString() ?? "Terjadi kesalahan"
+      };
+    }
+  }
+
+  Future<Map<String, dynamic>> register(String name, String email, String password, String? profilePath)async{
+    try{
+      MultipartFile? profileFile;
+      if(profilePath != null){
+        profileFile = await MultipartFile.fromFile(profilePath);
+      }
+      final request = FormData.fromMap({
+        "name": name,
+        "email": email,
+        "password": password,
+        if(profilePath != null)
+        "profile": profileFile
+      });
+      final response = await dio.post('/register', data: request);
+      if(response.data['success'] == true && response.statusCode == 201){
+        final key = await SharedPreferences.getInstance();
+        await key.setString("token", response.data['data']['token']);
+        await key.setBool("statusLogin", true);
+      }
+      return response.data;
+    }on DioException catch(e){
+      return{
+        "success": false,
+        "message": e.response?.data['message'].toString() ?? "Terjadi kesalahan"
+      };
+    }
+  }
+
+  Future<User> currentUser()async{
+    try{
+      final response = await dio.get('/user');
+      return User.fromJson(response.data);
+    }on DioException catch(e){
+      throw Exception(e);
+    }
+  }
+
+  Future<List<User>> getAllUser()async{
+    try{
+      final response = await dio.get('/message');
+      return (response.data['data'] as List).map((item) => User.fromJson(item)).toList();
+    }on DioException catch(e){
+      throw Exception(e.response);
+    }
+  }
+
+  Future<Map<String, dynamic>> getAllMessage(int receiverId)async{
+    try{
+      final response = await dio.get('/message/$receiverId');
+      return response.data;
+    }on DioException catch(e){
+      return{
+        "success": false,
+        "message": e.response?.data['message'].toString() ?? "Terjadi kesalahan"
+      };
+    }
+  }
+
+  Future<Map<String, dynamic>> sendMessage(int chatRoomId, String message, String? imagePath)async{
+    try{
+      MultipartFile? imageFile;
+      if(imagePath != null){
+        imageFile = await MultipartFile.fromFile(imagePath);
+      }
+      final request = FormData.fromMap({
+        "chat_room_id": chatRoomId,
+        "message": message,
+        if(imagePath != null)
+        "image": imageFile
+      });
+      final response = await dio.post("/message", data: request);
+      return response.data;
+    }on DioException catch(e){
+      return{
+        "success": false,
+        "message": e.response?.data['message'].toString() ?? "Terjadi kesalahan"
+      };
+    }
+  }
+
+  Future<Map<String, dynamic>> authBroadcast(String? socketId, String? channelName)async{
+    try{
+      final response = await dio.post('/broadcasting/auth', data: {
+        "socket_id": socketId,
+        "channel_name": channelName
+      });
+      return response.data;
+    }on DioException catch(e){
+      throw Exception(e);
     }
   }
 }
